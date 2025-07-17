83073e3f5873e5443e79188b67b38e16368bbc41
gandli <gandli@qq.com>
2025-07-17 21:30:09 +0800

    flclash 0.8.86

--- Casks/flclash.rb
+++ Casks/flclash.rb
@@ -2,7 +2,7 @@ cask "flclash" do
   arch arm: "arm64", intel: "amd64"
 
   version "0.8.86"
-  sha256 arm: "9921b38cf6bf9d2c22893bb748f792c6932339a7906d8db51f0c50bd9f61e8fa",
+  sha256 arm:   "9921b38cf6bf9d2c22893bb748f792c6932339a7906d8db51f0c50bd9f61e8fa",
          intel: "9921b38cf6bf9d2c22893bb748f792c6932339a7906d8db51f0c50bd9f61e8fa"
 
   url "https://github.com/chen08209/FlClash/releases/download/v#{version}/FlClash-#{version}-macos-#{arch}.dmg"
