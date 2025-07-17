5a10c44929e7bf3737ea40d48066d2afbe9a07e8
gandli <gandli@qq.com>
2025-07-17 21:32:56 +0800

    fix: 更新intel架构的sha256校验值

--- Casks/flclash.rb
+++ Casks/flclash.rb
@@ -3,7 +3,7 @@ cask "flclash" do
 
   version "0.8.86"
   sha256 arm: "9921b38cf6bf9d2c22893bb748f792c6932339a7906d8db51f0c50bd9f61e8fa",
-         intel: "9921b38cf6bf9d2c22893bb748f792c6932339a7906d8db51f0c50bd9f61e8fa"
+         intel: "cfd733d54b3520cc5f2a38ab02d6dc06185182fafd0861e334becbd568ea45c9"
 
   url "https://github.com/chen08209/FlClash/releases/download/v#{version}/FlClash-#{version}-macos-#{arch}.dmg"
   name "FlClash"
